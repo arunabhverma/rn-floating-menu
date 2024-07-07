889667844df51a61e73e8b50b2cf0dccfc4fd8c7
Arunabh verma <arunabhverma@Arunabhs-MacBook-Air.local>
2024-07-07 12:55:19 +0530

    fixed colors for android

--- components/CustomBlurView.tsx
+++ components/CustomBlurView.tsx
@@ -1,24 +1,31 @@
 import {
+  Platform,
   StyleSheet,
-  Text,
   useColorScheme,
   useWindowDimensions,
-  View,
 } from "react-native";
 import React from "react";
 import { BlurView } from "expo-blur";
+import { useTheme } from "@react-navigation/native";
 
 const CustomBlurView = ({ bgColor }: { bgColor?: string }) => {
   const tint = useColorScheme();
+  const theme = useTheme();
+
   const { width, height } = useWindowDimensions();
   return (
     <BlurView
       style={[
         StyleSheet.absoluteFillObject,
-        { width, height, backgroundColor: bgColor },
+        {
+          width,
+          height,
+          backgroundColor:
+            Platform.OS === "android" ? theme.colors.border : bgColor,
+        },
       ]}
       tint={tint === "dark" ? "systemMaterialDark" : "systemMaterialLight"}
-      intensity={100}
+      intensity={Platform.OS === "android" ? 0 : 100}
     />
   );
 };
